c4b453fcf01ff8df1b1cf524c3f120792029807f
Thang Tran <thangtv611@gmail.com>
2023-12-11 20:37:49 +0700

    custom keymap + fix telescope error

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,11 @@
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+-- vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+-- vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+-- vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+-- vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- faster Up/Down with J/K
+vim.keymap.set('n', '<c-j>','5j<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<c-k>','5k<CR>', { noremap = true, silent = true })
